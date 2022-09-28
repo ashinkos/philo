70cd30e90b0c2e5827489d59eb73c4a923fae501
Alaa Aouni <aaouni@e2r1p1.1337.ma>
2022-09-28 21:55:48 +0100

    second

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: aaouni <aaouni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 19:46:47 by aaouni            #+#    #+#             */
-/*   Updated: 2022/09/28 21:49:29 by aaouni           ###   ########.fr       */
+/*   Updated: 2022/09/28 21:55:08 by aaouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,47 +31,46 @@ int	ft_atoi(const char *str)
 	return (sign * nb);
 }
 
-t_plist *fill_argument(int ac, char **av)
+t_plist	*fill_argument(int ac, char **av)
 {
-    t_plist *lst;
-    
-    lst = malloc(sizeof(t_plist));
-    if (!lst)
-        return (lst);
-    if (ac > 4)
-    {
-        lst->nbr_philo = ft_atoi(av[1]);
-        lst->die = ft_atoi(av[2]);
-        lst->eat = ft_atoi(av[3]);
-        lst->sleep = ft_atoi(av[4]);
-        lst->nbr_eat = 0;
-    }
-    if (ac == 6)
-        lst->nbr_eat = ft_atoi(av[5]);
-    return (lst);
+	t_plist	*lst;
+
+	lst = malloc(sizeof(t_plist));
+	if (!lst)
+		return (lst);
+	if (ac > 4)
+	{
+		lst->nbr_philo = ft_atoi(av[1]);
+		lst->die = ft_atoi(av[2]);
+		lst->eat = ft_atoi(av[3]);
+		lst->sleep = ft_atoi(av[4]);
+		lst->nbr_eat = 0;
+	}
+	if (ac == 6)
+		lst->nbr_eat = ft_atoi(av[5]);
+	return (lst);
 }
 
-void    print_lst(t_plist *lst)
+void	print_lst(t_plist *lst)
 {
-    printf("number of philo : %d \n", lst->nbr_philo);
-    printf("time to die : %d \n", lst->die);
-    printf("time to eat : %d \n", lst->eat);
-    printf("time to sleep : %d \n", lst->sleep);
-    printf("number of time to eat : %d \n", lst->nbr_eat);
+	printf("number of philo : %d \n", lst->nbr_philo);
+	printf("time to die : %d \n", lst->die);
+	printf("time to eat : %d \n", lst->eat);
+	printf("time to sleep : %d \n", lst->sleep);
+	printf("number of time to eat : %d \n", lst->nbr_eat);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_plist *lst;
-    
-    lst = NULL;
-    
-    if (ac < 5 || ac > 6)
-    {
-        printf("nombre of argument is invalid\n");
-        exit(1);
-    }
-    lst = fill_argument(ac, av);
-    print_lst(lst);
-    av[0] = av[0]++;
+	t_plist	*lst;
+
+	lst = NULL;
+	if (ac < 5 || ac > 6)
+	{
+		printf("nombre of argument is invalid\n");
+		exit(1);
+	}
+	lst = fill_argument(ac, av);
+	print_lst(lst);
+	av[0] = av[0]++;
 }
